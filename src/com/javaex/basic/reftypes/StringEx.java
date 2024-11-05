7a1c7162a90d20819dbf03372ab6c97919903b50
leegeonmin <l_lpp@naver.com>
2024-11-05 11:56:55 +0900

    string format 연습

--- src/com/javaex/basic/reftypes/StringEx.java
+++ src/com/javaex/basic/reftypes/StringEx.java
@@ -3,7 +3,8 @@ package com.javaex.basic.reftypes;
 public class StringEx {
 
 	public static void main(String[] args) {
-		usingString();
+//		usingString();
+		stringFormatEx();
 
 	}
 
@@ -24,7 +25,41 @@ public class StringEx {
 	}
 	
 	
-	
+	private static void stringFormatEx() {
+		// 포매팅 문자 중에서 %s(문자열), %d(정수), %n(개행)
+		
+		// 만약에'10개의 사과 중에 3개를 먹었다'라는 문장을 만들고 싶다
+		// 10의 자리에 %d , 사과 자리에 %s , 3개 자리에 %d
+		
+		int total = 10, eat = 7;
+		String fruit = "사과";
+		System.out.println(total + "개의 " + fruit + "중에 " + eat + "개를 먹었다");
+		// 이전에 사용하던 방식
+		
+		// formatting 을 이용한 방식
+		System.out.printf("%d개의 %s중에 %d개를 먹었다%n", total, fruit, eat);
+		
+		// string.format 매서드를 활용한 것
+		String template = "%d개의 %s중에 %d개를 먹었다";
+		System.out.println(String.format(template, 10, "사과", 5));
+		
+		System.out.println(String.format(template, 5, "바나나", 2));
+		
+		
+		
+		// 천단위에 , 포매팅
+		int amount = 123_456_789;
+		System.out.printf("현재 잔고는: %d입니다.%n", amount);
+		System.out.printf("현재 잔고는: %,d입니다.%n", amount);
+//		System.out.println("현재 잔고는: %,d입니다.%n", amount);		// 프린트 부분 보면 위에는 printf 로 썼고 얘는 println로 써서 오류난거임
+		
+		
+		// %f
+		float interestRate = 1.2345f;
+		System.out.printf("현재 이자율을 %f입니다.%n", interestRate);
+		System.out.printf("현재 이자율을 %.2f입니다.%n", interestRate);
+			
+	}
 	
 	
 	

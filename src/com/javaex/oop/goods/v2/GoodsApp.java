7f5b116e6655ed3650e302fb6905daec8cf16f41
leegeonmin <l_lpp@naver.com>
2024-11-07 11:09:34 +0900

    Getter Setter

--- src/com/javaex/oop/goods/v2/GoodsApp.java
+++ src/com/javaex/oop/goods/v2/GoodsApp.java
@@ -4,11 +4,36 @@ package com.javaex.oop.goods.v2;
 // Goods v2. 접근 제한자
 class Goods {					// 제한자를 따로 설정하지 않았지? 이걸 디폴트로 보는거임
 	// 필드 선언	-> private로 변경 
+	// >> 정보는 은닉하고 Getter와 Setter 를 통한 우회 접근이 필요!
 	private String name;
 	private int price;
 	// private 추가해주니까, 밑에 싹 다 에러남. 공유안되니까
-	// 해결하는 방법? 있다!! ㄱㄷ
+	// 해결하는 방법? 있다!! ㄱㄷ  -> 우회 접근
+	
+	// name 필트를 위한 Getter 
+	public String getName() {
+		return name;
+	}
+	
+	// name 필드를 위한 Setter
+	public void setName(String name) {
+		this.name = name;
+		// this -> 현재 인스턴스 자체
+		
+	}
+	
+	// price 필드를 위한 Getter  
+	public int getPrice() {
+		return price;
+	}
+	
+	// price 필드를 위한 Setter 
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
 }
+
 
 // Goods 테스트 클래스
 public class GoodsApp {
@@ -16,22 +41,25 @@ public class GoodsApp {
 	public static void main(String[] args) {
 		Goods camera = new Goods();		// 생성자 호출
 		// 생성자를 호출 -> 인스턴스 생성 -> camera로 참조
-		camera.name = "Nikon";
-		camera.price = 400_000;
-		System.out.printf("%s -> %,d%n", camera.name, camera.price);
+//		camera.name = "Nikon";
+//		camera.price = 400_000;
+		camera.setName("Nikon");
+		camera.setPrice(400_000);
+		
+//		System.out.printf("%s -> %,d%n", camera.name, camera.price);
+		System.out.printf("%s -> %,d%n", camera.getName(), camera.getPrice());
 
 		
 		Goods on = new Goods();
-		on.name = "LG그램";
-		on.price = 900_000;
-		System.out.printf("%s -> %,d%n", on.name, on.price);
+		on.setName("LG그램");
+		on.setPrice(900_000);
+		System.out.printf("%s -> %,d%n", on.getName(), on.getPrice());
 		
 		
 		Goods mug = new Goods();
-		mug.name = "머그컵";
-		mug.price = 2_000;
-		System.out.printf("%s -> %,d%n", mug.name, mug.price);
-		
+		mug.setName("머그컵");
+		mug.setPrice(2_000);
+		System.out.printf("%s -> %,d%n", mug.getName(), mug.getPrice());
 	}
 	
 	

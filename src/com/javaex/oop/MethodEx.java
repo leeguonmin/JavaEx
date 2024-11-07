534f4d6a3eefdeec1c56719dcb48bb7501458ea7
leegeonmin <l_lpp@naver.com>
2024-11-07 10:19:36 +0900

    가변인수

--- src/com/javaex/oop/MethodEx.java
+++ src/com/javaex/oop/MethodEx.java
@@ -9,27 +9,59 @@ public class MethodEx {
 	public static void main(String[] args) {
 		// 1.
 		// 매개변수가 없고 리턴도 없다 (매개변수 x, 리턴 x)
-		printMessage();
+//		printMessage();
 		
 		
 		// 2.
 		// 매개변수 x, 리턴 o
-		String message = getMessage();
-		System.out.println("getMessage:"+ message);
+//		String message = getMessage();
+//		System.out.println("getMessage:"+ message);
 		
 		
 		// 3.
 		// 매개변수 o, 리턴 x
-		printSum(10,20);
+//		printSum(10,20);
 		
 		// 4.
 		// 매개변수 o, 리턴 o
-		System.out.println(getSum(10,20));
-		// 5.
-		System.out.println(getSum(10,20,30));
+//		System.out.println(getSum(10,20));
+		// 5.매개변수 o, 리턴 o (오버로그. 중복 권유)
+//		System.out.println(getSum(10,20,30));
 		
+		
+		// 6. 매개 변수 개수를 알 수 없을 때의 해결 방법
+		// 방법 1, 배열을 전달한다
+		System.out.println("getSumArr:"+getSumArr(new double[] {1,2,3,4,5,6,7,8}));
+		// 방법 2, 가변 인수 
+		System.out.println("getSumVar:" +getSumVar(1,2,3,4,5,6,7,8));
 	}
+	
+	// 6.
+	private static double getSumArr (double[] values) {
+		// 방법 1 . 배열을 전달한다
+		double total = 0;
+		
+		for (int i = 0; i<values.length; ++i) {
+			total += values[i];
+		}
+		return total;
 
+	}
+	
+	private static double getSumVar(double...valuse) {
+		// 방법2. 가변 인수 
+		double total =0;
+		
+		for(double value: valuse) {
+			total+=value;
+		}
+		return total;
+	}
+	
+	
+	
+	
+	
 	// 1.
 	// 매개변수 x	-> 파라미터 없음 
 	// 리턴 x 	-> void

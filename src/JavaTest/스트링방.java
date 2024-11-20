d1db328854e4e6bd18c6cea8cc7a5a25660b5ab0
leegeonmin <l_lpp@naver.com>
2024-11-20 11:14:36 +0900

    Create 스트링방.java
    커밋

---
+++ src/JavaTest/스트링방.java
@@ -0,0 +1,19 @@
+package JavaTest;
+
+import: java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String[] names) {
+        List<String> aw = new ArrayList<>();
+        
+        for (int i = 0; i < names.length; i += 5) {
+                aw.add(names[i]);
+        }
+        
+        String[] cf = new String[aw.size()];
+        // 스트링 방을 새로 만들어주라고. aw 사이즈 만하게
+            return aw.toArray(cf);
+ 
+
+    }
+}
